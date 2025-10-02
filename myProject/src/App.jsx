508b01bef662a8111583a38bac9118489e1a9187
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-10-02 18:45:59 +0500

    First Project

---
+++ myProject/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MyComponent from './Component/MyComponent'
+function App() {
+  return (
+    <>
+      <MyComponent myName="Suleman" />
+      <MyComponent myName="Ali" />
+      <MyComponent myName="Ahmed" />
+      <MyComponent myName="Usman" />
+    </>
+  )
+}
+
+export default App
