5075d26beb8c8975c002f5d6840a78a10b4d6baf
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-10-07 18:41:22 +0500

    Props

--- myProject/src/App.jsx
+++ myProject/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import MyComponent from './Component/MyComponent'
+
 function App() {
   return (
     <>
-      <MyComponent myName="Suleman" />
-      <MyComponent myName="Ali" />
-      <MyComponent myName="Ahmed" />
-      <MyComponent myName="Usman" />
+    <MyComponent myName="John"/>
+    <MyComponent myName="ALi"/>
+    <MyComponent myName="Usman"/>
     </>
   )
 }
