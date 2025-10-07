d60899aec8142f2c986c299d039bbb0e2c285102
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-10-07 18:52:08 +0500

    Practice

--- new_app/src/Navbar/Navbar.jsx
+++ new_app/src/Navbar/Navbar.jsx
@@ -3,14 +3,16 @@ import './Navbar.css'
 
 function Navbar() {
   return (
-    <nav>
+        <>
+        <nav>
         <h1>This is Navbar</h1>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
+          <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+          </ul>
+      </nav>
+      </>
   )
 }
 
