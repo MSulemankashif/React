4710c2ed3910979596d2de7f5160333c5fd73a55
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-10-18 18:53:22 +0500

    Practice

--- useState Practice/src/App.jsx
+++ useState Practice/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ControlledExample from './Components/ControlledExample'
+import Counter from './Components/Counter'
 
 function App() {
 
   return (
     <>
-      <ControlledExample/>
+      <Counter/>
     </>
   )
 }
